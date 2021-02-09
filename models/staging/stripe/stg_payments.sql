886a10f692d52994d2b62fadd8d2ec9e7379ca65
Michael Moyer <mimoyer21@gmail.com>
2021-02-09 21:11:24 +0000

    Adding all changes made during training course through Packages section

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,6 +3,6 @@ select id as payment_id
     , paymentmethod as payment_method
     , status
     -- amount is stored in cents, convert it to dollars
-    , amount / 100 as amount
+    , {{ cents_to_dollars('amount') }} as amount
     , created as created_at
 from {{ source('stripe','payment') }}
